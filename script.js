d42f0270a3f89240e4a2a955cf2a902378d1c92c
Maria Hernandez <Marialehdes@gmail.com>
2026-03-12 21:05:15 -0400

    Implement movie card creation and rendering
    Added functions to create and render movie cards.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("ReelTalk Movie Tracker Loaded");
+function createMovieCard(movie) {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+  card.dataset.id = movie.id;
+
+  card.innerHTML = `
+    <h3 class="movie-title">${movie.title}</h3>
+    <span class="movie-genre">${movie.genre}</span>
+  `;
+
+  return card;
+}
+
+function renderMovies(movieList) {
+  const container = document.getElementById("movie-list");
+  container.innerHTML = "";
+
+  movieList.forEach((movie) => {
+    container.appendChild(createMovieCard(movie));
+  });
+}
+
+renderMovies(movies);
