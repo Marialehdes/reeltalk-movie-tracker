467f023b46c872c7890b1813b69aa2073e2a1ed9
Maria Hernandez <Marialehdes@gmail.com>
2026-03-12 20:17:20 -0400

    Add movies data to movies.js

---
+++ data/movies.js
@@ -0,0 +1,32 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    genre: "Sci-Fi"
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    genre: "Action"
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    genre: "Sci-Fi"
+  },
+  {
+    id: 4,
+    title: "The Shawshank Redemption",
+    genre: "Drama"
+  },
+  {
+    id: 5,
+    title: "Parasite",
+    genre: "Thriller"
+  },
+  {
+    id: 6,
+    title: "Avengers: Endgame",
+    genre: "Action"
+  }
+];
